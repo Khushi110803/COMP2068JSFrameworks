2565d9f1e17bbd173a078b79aa9bcd001661d736
Khushi110803 <200539535@student.georgianc.on.ca>
2024-06-14 02:10:45 -0400

    done
    all webpages updated with my project links(which has been done in my previous years of study)

---
+++ ASSIGNEMNT01/README.md
@@ -0,0 +1,4 @@
+1. I have added Image gallery project, which is from my HTML and CSS Class
+2. To Do web app (to add my daily task)
+3. Rock, paper and scissor( from JSFrameworks LAB 2)
+4. Pizza website from my css project
---
+++ ASSIGNEMNT01/routes/projects.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET projects listing. */
+router.get("/", function (req, res, next) {
+  // projects data object
+  res.render("projects", {
+    projects: [
+      {
+        projectName: "Image Gallery Application",
+        projectDescription:
+          "A web-app based on HTML, CSS and JS which have beautiful graphics to show flowers.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+        link: "https://64b083a25ad959520e4cdd95--papaya-froyo-0cf997.netlify.app/",
+      },
+      {
+        projectName: "ToDo Web app",
+        projectDescription:
+          "A web-app based on Express and React which stores the tasks you have todo.",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+        link: "https://64cc26d720393422cda4aa78--sparkly-chaja-8923f5.netlify.app/",
+      },
+      {
+        projectName: "ROCK, PAPER AMD SCISSOR",
+        projectDescription:
+          "Guessing game : Each player throws their choices out on the count of three by calling out “rock, paper, scissors.”",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+        link: "https://khushi110803.github.io/COMP2068JSFrameworks/",
+      },
+      {
+        projectName: "Pizza Website",
+        projectDescription:
+          "A ecommer-app based on HTML, CSS and JS. Themed on Pizza orders and pizza website. ",
+        projectImage: "/images/laptop-g07bca914f_1280.png",
+        link: "https://khushi110803.github.io/HTML/",
+      },
+    ],
+  });
+});
+
+module.exports = router;
